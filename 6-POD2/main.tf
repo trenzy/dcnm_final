aaeeddebcb4eace984092b258f23c3d601e7d90a
Thomas Renzy <threnzy@cisco.com>
2021-04-20 19:27:02 -0700

    Update main.tf

--- 6-POD2/main.tf
+++ 6-POD2/main.tf
@@ -9,7 +9,8 @@ terraform {
 
 provider "dcnm" {
   # Configuration options
-  username = "admin"
-  password = "WWTwwt1!"
-  url      = "https://10.253.180.125"
+  username = "****"
+  password = "****"
+  url      = "https://****"
+}
 }
